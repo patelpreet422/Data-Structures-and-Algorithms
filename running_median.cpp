1300c410335071c7ffe3dafc8c4e158f36e9eb58
Preet Patel <patelpreet422@gmail.com>
2018-06-05 23:44:39 +0530

    Create running_median.cpp

---
+++ running_median.cpp
@@ -0,0 +1,73 @@
+#include <iostream>
+#include <queue>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+#include <cstdio>
+using namespace std;
+
+void addNumber(unsigned long i, priority_queue<unsigned long> &lower, priority_queue<unsigned long, vector<unsigned long>, greater<unsigned long>> &higher);
+float getMedian(priority_queue<unsigned long> &lower, priority_queue<unsigned long, vector<unsigned long>, greater<unsigned long>> &higher);
+void rebalance(priority_queue<unsigned long> &lower, priority_queue<unsigned long, vector<unsigned long>, greater<unsigned long>> &higher);
+
+void runningMedian(unsigned long *data, int size, float *medians){
+    priority_queue<unsigned long> lower;
+    priority_queue<unsigned long, vector<unsigned long>, greater<unsigned long>> higher;
+    for(int i = 0; i < size; ++i){
+        addNumber(data[i], lower, higher);
+        medians[i] = getMedian(lower, higher);
+    }
+}
+
+float getMedian(priority_queue<unsigned long> &lower, priority_queue<unsigned long, vector<unsigned long>, greater<unsigned long>> &higher) {
+    float median = 0;
+    if(lower.size() == higher.size()){
+        median = ((float)lower.top() + (float)higher.top())/2;
+    }else{
+        if(lower.size() > higher.size()){
+            median = lower.top();
+        }else{
+            median = higher.top();
+        }
+    }
+    return median;
+}
+
+void addNumber(unsigned long i, priority_queue<unsigned long> &lower, priority_queue<unsigned long, vector<unsigned long>, greater<unsigned long>> &higher) {
+    if(lower.empty() || i < lower.top()){
+        lower.push(i);
+    } else{
+        higher.push(i);
+    }
+    rebalance(lower, higher);
+}
+
+void rebalance(priority_queue<unsigned long> &lower, priority_queue<unsigned long, vector<unsigned long>, greater<unsigned long>> &higher) {
+    int64_t higherSize = higher.size();
+    int64_t lowerSize = lower.size();
+    if(abs(higherSize - lowerSize) > 1){
+        if(lower.size() > higher.size()){
+            higher.push(lower.top());
+            lower.pop();
+        }else{
+            lower.push(higher.top());
+            higher.pop();
+        }
+    }
+}
+
+int main()
+{
+    int size;
+    cin >> size;
+    unsigned long data[size];
+    float medians[size];
+    for(int i = 0; i < size; ++i){
+        cin >>  data[i];
+    }
+    runningMedian(data, size, medians);
+    for_each(medians, medians + size, [](const auto &m){
+        printf("%.1f\n", m);
+    });
+    return 0;
+}
