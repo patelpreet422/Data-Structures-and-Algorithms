b296d4697ec5676656088ff7f97824f0e1cd8fe6
Preet Patel <patelpreet422@gmail.com>
2018-06-08 02:17:30 +0530

    Update quick_sort.cpp

--- quick_sort.cpp
+++ quick_sort.cpp
@@ -1,35 +1,34 @@
-/*if you are using Visual Studio then uncomment the line below*/
-//#pragma warning(disable : 4996)
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
-int partition(int *array, int low, int high){
-    int pivot = array[(low + high)/2];
-    while(low<=high){
-        while(array[low] < pivot) ++low;
-        while(array[high] > pivot) --high;
-        if(low <= high) swap(array[low++], array[high--]);
-    }
-    return low;
+int partition(int *array, int low, int high) {
+	int pivot = array[(low + high) / 2];
+	while (low <= high) {
+		while (array[low] < pivot) ++low;
+		while (array[high] > pivot) --high;
+		if (low <= high) swap(array[low++], array[high--]);
+	}
+	return low;
 }
-void quickSortHelper(int *array, int low, int high){
-    if(low >= high) return;
-    int partitionPoint = partition(array, low, high);
-    quickSortHelper(array, low, partitionPoint-1);
-    quickSortHelper(array, partitionPoint, high);
+void quickSortHelper(int *array, int low, int high) {
+	if (low >= high) return;
+	int partitionPoint = partition(array, low, high);
+	quickSortHelper(array, low, partitionPoint - 1);
+	quickSortHelper(array, partitionPoint, high);
 }
-void quickSort(int *array, int size){
-    quickSortHelper(array, 0, size - 1);
+void quickSort(int *array, int size) {
+	quickSortHelper(array, 0, size - 1);
 }
 int main()
 {
-    int size = 10;
-    int array[size];
-    for(int i = 0; i < size; ++i){
-        array[i] = rand()%10 + 1;
-    }
-    quickSort(array, size);
-    for_each(array, array+size, [](const auto &e){cout << e << ' ';});
-    return 0;
+	int size = 10;
+	int *array = new int[size];
+	for (int i = 0; i < size; ++i) {
+		array[i] = rand() % 10 + 1;
+	}
+	quickSort(array, size);
+	for_each(array, array + size, [](const auto &e) {cout << e << ' '; });
+	delete[] array;
+	return 0;
 }
