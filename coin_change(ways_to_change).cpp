b4c8f31c0316cce6b59b508079281df6234c7694
Preet Patel <patelpreet422@gmail.com>
2018-06-22 00:17:01 +0530

    Update coin_change(ways_to_change).cpp

--- coin_change(ways_to_change).cpp
+++ coin_change(ways_to_change).cpp
@@ -1,38 +1,40 @@
 #include <iostream>
 #include <string>
-#include <unordered_set>
+#include <vector>
 using namespace std;
 
-int waysToChangeHelper(int coin, const unordered_set<int> &coins, unordered_set<int>::const_iterator current_it, string chosen)
+int waysToChangeHelper(int coin, const vector<int> &coins, int current_it, string &chosen)
 {
+	
 	if(coin == 0)
 	{
 		cout << chosen << '\n';
 		return 1;
 	}
 	int ways = 0;
-	for(auto it = current_it; it != coins.end();++it)
+	for(int i = current_it; i < coins.size(); ++i)
 	{
-		int current_coin = *it;
+		int current_coin = coins[i];
 		if(coin-current_coin>=0)
 		{
-			chosen += to_string(current_coin);
-			ways += waysToChangeHelper(coin-current_coin, coins, it, chosen);
-			chosen.erase(chosen.size()-1, 1);
+			chosen.push_back(current_coin+'0');
+			ways += waysToChangeHelper(coin-current_coin, coins, i, chosen);
+			chosen.pop_back();
 		}
 	}
 	return ways;
 }
 
-int waysToChange(int coin, const unordered_set<int> &coins)
+int waysToChange(int coin, const vector<int> &coins)
 {
-	return waysToChangeHelper(coin, coins, coins.begin(), "");
+    string chosen = "";
+	return waysToChangeHelper(coin, coins, 0, chosen);
 }
 
 int main()
 {
 	int coin = 5;
-	unordered_set<int> coins{1, 2, 5};
+	vector<int> coins{1, 2, 3, 4, 5, 6, 7, 8, 9};
 	cout << waysToChange(coin, coins) << '\n';
 	return 0;
 }
