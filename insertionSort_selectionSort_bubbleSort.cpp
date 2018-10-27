eb6b6f81fbc9d88d860248d48b1ae3e87b455580
Preet Patel <patelpreet422@gmail.com>
2018-10-27 13:58:56 +0530

    Update insertionSort_selectionSort_bubbleSort.cpp

--- insertionSort_selectionSort_bubbleSort.cpp
+++ insertionSort_selectionSort_bubbleSort.cpp
@@ -1,84 +1,49 @@
 #include <iostream>
 #include <algorithm>
-#include <climits>
+#include <vector>
 using namespace std;
 
-void insertionSort(int *array, int size)
+void insertion_sort(vector<int> & vec)
 {
-	for (int i = 1; i < size; ++i)
-	{
-		int j = i - 1;
-		while (array[j] > array[j+1] && j >= 0)
-		{
-			swap(array[j], array[j+1]);
-			--j;
-		}
-	}
+  for(size_t i = 0; i < vec.size(); ++i)
+  {
+    for(size_t j = i + 1; j < vec.size(); ++j)
+    {
+      if(vec[i] > vec[j])
+      {
+        swap(vec[i], vec[j]);
+      }
+    }
+  }
+} 
+
+void bubble_sort(vector<int> & vec)
+{
+  for(size_t i = 0; i < vec.size(); ++i)
+  {
+    for(size_t j = 0; j <= vec.size() - i - 1; ++j)
+    {
+      if(vec[j] > vec[j + 1])
+      {
+        swap(vec[j], vec[j + 1]);
+      }
+    }
+  }
 }
 
-void selectionSort(int *arr, int size)
+void selection_sort(vector<int> & vec)
 {
-	for (int i = 0; i <= size - 2; ++i)
-	{
-		int min = arr[i];
-		int min_index = i;
-		for (int j = i + 1; j <= size - 1; ++j)
-		{
-			if (arr[j] < min)
-			{
-				min = arr[j];
-				min_index = j;
-			}
-		}
-		swap(arr[i], arr[min_index]);
-	}
+  for(size_t i = 0; i < vec.size() - 1; ++i)
+  {
+    auto min_ptr = min_element(next(begin(vec), i), end(vec));
+    swap(vec[i], *min_ptr);
+  }
 }
-
-void bubbleSort(int *arr, int size)
-{
-	for (int i = 0; i <= size - 2; ++i)
-	{
-		for (int j = i + 1; j <= size - 1; ++j)
-		{
-			if (arr[i] > arr[j])
-			{
-				swap(arr[i], arr[j]);
-			}
-		}
-	}
-}
-
 
 int main()
 {
-	int size = 100;
-	int *arr = new int[size];
-	for (int i = 0; i < size; ++i)
-	{
-		arr[i] = rand()%10 + 1;
-	}
-	
-	insertionSort(arr, size);
-	for (int i = 0; i < size; ++i)
-	{
-		cout << arr[i] << ' ';
-	}
-	cout << '\n';
-	
-	selectionSort(arr, size);
-	for (int i = 0; i < size; ++i)
-	{
-		cout << arr[i] << ' ';
-	}
-	cout << '\n';
-	
-	bubbleSort(arr, size);
-	for (int i = 0; i < size; ++i)
-	{
-		cout << arr[i] << ' ';
-	}
-	cout << '\n';
-	
-	delete[] arr;
-	return 0;
+  vector<int> vec{7, 8, 1, 0, 2, -7};
+  selection_sort(vec);
+  for(auto i: vec) {cout << i << '\n';}
+  return 0;
 }
