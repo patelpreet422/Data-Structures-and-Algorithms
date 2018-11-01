bc356f2439c30a84b5675372f1373cc9b5712ce2
Preet Patel <patelpreet422@gmail.com>
2018-11-01 18:05:46 +0530

    Update heap in linear time.cpp

--- heap in linear time.cpp
+++ heap in linear time.cpp
@@ -5,30 +5,20 @@ using namespace std;
 
 void heapify_down(vector<int> & vec, size_t i)
 {
-
-  bool swapped = true;
-  while (i < vec.size() && swapped) {
-      size_t left_child = 2*i + 1;
-      size_t right_child = 2*i + 2;
-      //bool swapped = false;
-      if(left_child < vec.size() && right_child < vec.size() && vec[left_child] < vec[right_child])
-       {
-           swap(vec[i], vec[right_child]);
-           i = right_child;
-        } else if(left_child < vec.size()) {
-          //only left child
-          if(vec[i] < vec[left_child])
-            {
-              swap(vec[i], vec[left_child]);
-              i = left_child;
-            } else {
-              swapped = false;
-            }
-        } else {
-          //no child
-          swapped = false;
-        }
-    }
+  while (2*i+1 < vec.size()) {
+  	int max_child = 2*i+1;
+      if(2*i+2 < vec.size() && vec[2*i+2] > vec[2*i+1])
+      {
+      	max_child = 2*i+2;
+      }
+      if(vec[i] < vec[max_child])
+      {
+      	swap(vec[i], vec[max_child]);
+      	i = max_child;
+      } else {
+      	return;
+      }
+  }
 }
 
 void build_max_heap(vector<int> & vec)
@@ -42,9 +32,9 @@ void build_max_heap(vector<int> & vec)
 int main()
 {
   vector<int> vec{-7, -7, 0, -5, 10, 5, 50};
-//  make_heap(begin(vec), end(vec));
+  //make_heap(begin(vec), end(vec));
   build_max_heap(vec);
-//  sort_heap(begin(vec), end(vec));
+ sort_heap(begin(vec), end(vec));
   for(auto i: vec) { cout << i << '\n'; }
   return 0;
 }
