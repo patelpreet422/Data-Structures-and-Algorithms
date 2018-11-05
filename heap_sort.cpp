b04ceab39a412479c65420b2ae6e9da71a541edb
Preet Patel <patelpreet422@gmail.com>
2018-11-05 12:24:14 +0530

    Update heap_sort.cpp

--- heap_sort.cpp
+++ heap_sort.cpp
@@ -1,89 +1,80 @@
-/*if you are using Visual Studio then uncomment the line below*/
-//#pragma warning(disable : 4996)
 #include <iostream>
-//#include <algorithm>
-#include <cstdlib>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <set>
 using namespace std;
-class Heap {
-private:
-    int *arr;
-    int size = 0;
-    int capacity = 2;
-    int getLeftChildIndex(int parentIndex) {return parentIndex*2+1;};
-    int getRightChildIndex(int parentIndex) {return parentIndex*2+2;};
-    int getParentIndex(int childIndex){return (childIndex-1)/2;}
-    bool hasLeftChild(int parentIndex){return getLeftChildIndex(parentIndex) < size;}
-    bool hasRightChild(int parentIndex){return getRightChildIndex(parentIndex) < size;}
-    bool hasParent(int childIndex){return getParentIndex(childIndex) >= 0;}
-    int leftChild(int parentIndex){return arr[getLeftChildIndex(parentIndex)];}
-    int rightChild(int parentIndex){return arr[getRightChildIndex(parentIndex)];}
-    int parent(int childIndex){return arr[getParentIndex(childIndex)];}
 
-    void heapifyUp() {
-        int currentChild = size-1;
-        while(hasParent(currentChild) && (parent(currentChild) >  arr[currentChild])){
-            swap(arr[currentChild], arr[getParentIndex(currentChild)]);
-            currentChild = getParentIndex(currentChild);
-        }
-    }
+void heapify_down(vector<int> & vec, int i)
+{
+  while (2*i+1 < vec.size()) {
+  	int max_child = 2*i+1;
+      if(2*i+2 < vec.size() && vec[2*i+2] > vec[2*i+1])
+      {
+      	max_child = 2*i+2;
+      }
+      if(vec[i]<vec[max_child])
+      {
+      	swap(vec[i], vec[max_child]);
+      	i = max_child;
+      } else {
+      	return;
+      }
+  }
+}
 
-    void heapifyDown(){
-        int currentParent = 0;
-        while(hasLeftChild(currentParent)){
-            int smallest_index = getLeftChildIndex(currentParent);
-            if(hasRightChild(currentParent) && (rightChild(currentParent) < leftChild(currentParent))){
-                smallest_index = getRightChildIndex(currentParent);
-            }
-            if(arr[currentParent] >= arr[smallest_index]){
-                swap(arr[smallest_index], arr[currentParent]);
-            } else {
-                break;
-            }
-            currentParent = smallest_index;
-        }
-    }
-public:
-    Heap(){
-        arr = new int[2];
-    }
-    ~Heap(){
-        delete arr;
-    }
+void heapify_up(vector<int> &vec, int i)
+{
+	int par = (i-1)/2;
+	while(i != 0 && vec[par] < vec[i])
+	{
+		swap(vec[i], vec[par]);
+		i = par;
+	}
+}
 
-    void add(int key){
-        if(size+1 > capacity)
-        {
-            capacity *= 2;
-            int *temp_array = new int[capacity];
-            copy(arr, arr+size, temp_array);
-            arr = temp_array;
-        }
-        arr[size] = key;
-        ++size;
-        heapifyUp();
-    }
-    int pull(){
-        int root_value = arr[0];
-        arr[0] = arr[size-1];
-        --size;
-        heapifyDown();
-        return root_value;
-    }
-};
+void insert_heap(vector<int> &vec, int k)
+{
+	vec.push_back(k);
+	heapify_up(vec, vec.size()-1);
+}
 
-int main() {
-    Heap h;
-    int size = 10;
-    for(int i = 1; i <= size; ++i)
-    {
-        int temp = rand()%10+1;
-        cout << temp << ' ';
-        h.add(temp);
-    }
-    cout << '\n';
+void build_max_heap(vector<int> &vec)
+{
+	for(int i = (vec.size()-2)/2; i >= 0; --i)
+	{
+		heapify_down(vec, i);
+	}
+}
 
-    for(int i = 1; i <= size; ++i) {
-        cout << h.pull() << ' ';
-    }
-    return 0;
+int delete_heap(vector<int> &vec)
+{
+	int k = vec[0];
+	vec[0] = vec[vec.size()-1];
+	vec.pop_back();
+	heapify_down(vec, 0);
+	return k;
+}
+
+void heap_sort(vector<int> &vec)
+{
+	vector<int> sorted_vec(vec.size());
+	build_max_heap(vec);
+	int size = vec.size();
+	for(int i = 0; i < size; ++i)
+	{
+		sorted_vec[size-i-1] = delete_heap(vec);
+	}
+	vec = sorted_vec;
+}
+
+int main()
+{
+  vector<int> vec{7, 10, 4, 3, 20, 15};
+  //make_heap(begin(vec), end(vec));
+  build_max_heap(vec);
+  //sort_heap(vec.begin(), vec.end());
+  heap_sort(vec);
+  for(auto i: vec) { cout << i << '\n'; }  
+  return 0;
 }
