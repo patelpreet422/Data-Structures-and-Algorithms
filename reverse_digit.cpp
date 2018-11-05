b9e807e44cf82ffd8dfc9cd88366a2ce2c42e55c
Preet Patel <patelpreet422@gmail.com>
2018-11-05 12:21:32 +0530

    Create reverse_digit.cpp

---
+++ reverse_digit.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int reverse_int(int k)
+{
+  int i = 0;
+  while(k)
+    {
+      i = i*10 + k%10;
+      k /= 10;
+    }
+  return i;
+}
+
+int main()
+{
+  cout << reverse_int(123456) << endl;
+  return 0;
+}
