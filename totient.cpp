5ce13a74beec52f4a2ef8812f815f904ed3ab62f
Preet Patel <patelpreet422@gmail.com>
2019-04-04 23:34:14 +0530

    Create totient.cpp

---
+++ totient.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <numeric>
+#include <bitset>
+#include <cmath>
+#include <map>
+using namespace std;
+using ul = unsigned long;
+
+vector<ul> prime_factors(ul k)
+{
+    vector<ul> p_factors;
+    for(int i = 2; i*i <= k; ++i){
+        bool isfactor = false;
+        while(k%i == 0){
+            k /= i;
+            isfactor = true;
+        }
+        if(isfactor) p_factors.push_back(i);
+    }
+    if(k != 1)
+        p_factors.push_back(k);
+    return p_factors;
+}
+
+ul totient(ul k)
+{
+    ul r = k;
+    auto p_factors = prime_factors(k);
+    for(auto e: p_factors)
+    {
+        r = r*(e-1)/e;
+    }
+    return r;
+}
+
+int main()
+{
+    for(auto e: prime_factors(15)) cout << e << ' ';
+    return 0;
+}
