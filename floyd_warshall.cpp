55ed6f7422b11d594f44bd108702dbb2e15023e1
Preet Patel <patelpreet422@gmail.com>
2019-06-05 09:51:00 +0530

    Update floyd_warshall.cpp

--- floyd_warshall.cpp
+++ floyd_warshall.cpp
@@ -1,101 +1,98 @@
-#include <numeric>
-#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <iomanip>
+#include <unordered_map>
+#include <map>
+#include <unordered_set>
+#include <stack>
+#include <limits>
+#include <queue>
 
 using namespace std;
+using vertex_t = long;
+using weight_t = int;
+using adjacency_matrix_t = vector<vector<vertex_t>>;
 
-
-pair<vector<vector<long>>, vector<vector<long>>> all_pair_sp(const vector<vector<long>>& graph)
+struct Graph
 {
-    const int INF = numeric_limits<int>::max();
+    adjacency_matrix_t adjacency_matrix;
+    size_t vertices;
 
-    long n = graph.size();
-    vector<vector<long>> path = graph;
-
-    // pred stores the next vertex where we must move to reach from i->j
-    vector<vector<long>> pred(n, vector<long>(n, INF));
-
-    for(auto i = 0; i < n; ++i)
+    Graph() = delete;
+    Graph(vector<vector<vertex_t>> am): adjacency_matrix(move(am)), vertices(am.size()) {}
+    Graph(size_t s): vertices(s)
     {
-        for(auto j = 0; j < n; ++j)
+        adjacency_matrix.resize(s, vector<vertex_t>(s, 0));
+    }
+
+    void add_edge(vertex_t v1, vertex_t v2, weight_t weight = 1, bool directed = true)
+    {
+        adjacency_matrix[v1][v2] = weight;
+        if(!directed) adjacency_matrix[v2][v1] = weight;
+    }
+
+    vector<vertex_t> neighbours(vertex_t v)
+    {
+        vector<vertex_t> children;
+        for(vertex_t i = 0; i < vertices; ++i)
         {
-            if(graph[i][j] != INF) pred[i][j] = j;
+            if(adjacency_matrix[v][i]) children.push_back(i);
+        }
+        return children;
+    }
+};
+
+adjacency_matrix_t floyd_warshall(Graph g, vertex_t s, vertex_t t)
+{
+    adjacency_matrix_t path_matrix = g.adjacency_matrix;
+    vector<vector<vertex_t>> parent(g.vertices, vector<vertex_t>(g.vertices, numeric_limits<int>::max()));
+
+    // path matrix setup
+    for(vertex_t i = 0; i < g.vertices; ++i)
+    {
+        for (vertex_t j = 0; j < g.vertices; ++j)
+        {
+            if(g.adjacency_matrix[i][j] != 0) parent[i][j] = j;
+            if(path_matrix[i][j] == 0) path_matrix[i][j] = numeric_limits<int>::max();
+            if(i == j) path_matrix[i][j] = 0;
         }
     }
 
-    for(auto k = 0; k < n; ++k)
+    // actual algo
+    for(vertex_t k = 0; k < g.vertices; ++k)
     {
-        for(auto i = 0; i < n; ++i)
+        for(vertex_t i = 0; i < g.vertices; ++i)
         {
-            for(auto j = 0; j < n; ++j)
+            for(vertex_t j = 0; j < g.vertices; ++j)
             {
-                if(path[i][k]+path[k][j] < path[i][j])
+                if(path_matrix[i][j] > path_matrix[i][k]+path_matrix[k][j])
                 {
-                    path[i][j] = path[i][k]+path[k][j];
-                    pred[i][j] = k;
+                    path_matrix[i][j] = path_matrix[i][k]+path_matrix[k][j];
+                    parent[i][j] = k;
                 }
             }
         }
     }
-    return make_pair(path, pred);
+
+    // print path
+    cout << s << " ";
+    for(vertex_t current_vertex = parent[s][t]; current_vertex != numeric_limits<int>::max(); 
+        current_vertex = parent[current_vertex][t])
+        cout << current_vertex << " ";
+
+    return path_matrix;
 }
 
-//iterative
-void construct_path(const vector<vector<long>>& pred, long s, long e)
-{
-    cout << s << " -> ";
-    for(auto at = pred[s][e]; at!=e; at=pred[at][e]) {
-        cout << at << " -> ";
-    }
-    cout << e << '\n';
-
-    //pass path as well to return cost
-    //return path[s][e];
-}
-
-//recursive
-void path_reconstruction(const vector<vector<long>>& pred, long s, long e)
-{
-    if(pred[s][e] == e)
-    {
-        cout << s << " -> ";
-    } else {
-        path_reconstruction(pred, s, pred[s][e]);
-        path_reconstruction(pred, pred[s][e], e);
-    }
-}
-
-void pretty_print_path(const vector<vector<long>>& pred, long s, long e)
-{
-    path_reconstruction(pred, s, e);
-    cout << e << '\n';
-}
-
-//driver code
 int main() {
-    const int INF = numeric_limits<int>::max();
-    vector<vector<long>> vec { {0,   5,  INF, 10},
-                               {INF,  0,  3,  INF},
-                               {INF, INF, 0,   1},
-                               {INF, INF, INF, 0} };
+    Graph g(5);
 
-    auto [path, pred] = all_pair_sp(vec);
+    for(int i = 0; i < 7; ++i)
+    {
+        int v1, v2, w;
+        cin >> v1 >> v2 >> w;
+        g.add_edge(v1, v2, w);
+    }
 
-//    for(auto& r: path)
-//    {
-//        for(auto e: r)
-//        {
-//            cout << setw(5) << ((e==INF)?"INF":to_string(e)) << " ";
-//        }
-//        cout << '\n';
-//    }
-    construct_path(pred, 1, 3);
-    //pretty_print_path(pred, 0, 3);
+    auto apsp = floyd_warshall(g, 2, 1);
+
     return 0;
 }
-
-// go 0 -> 3, cost = 8
-// 0 -> 1 -> 2 -> 3
-
