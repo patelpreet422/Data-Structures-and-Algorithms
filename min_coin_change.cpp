5b09c7ae6946b381970ae1c0ae86965b09d64968
Preet Patel <patelpreet422@gmail.com>
2019-06-07 02:39:28 +0530

    Update min_coin_change.cpp

--- min_coin_change.cpp
+++ min_coin_change.cpp
@@ -1,10 +1,32 @@
-int min_changes(const vector<int>& vec, int target)
+#include <iostream>
+#include <vector>
+#include <limits>
+#include <algorithm>
+
+using namespace std;
+
+int minimum_coin_change(vector<int> coins, int target)
 {
-    if(target == 0) return 0;
-    int m_c = numeric_limits<int>::max();
-    for(auto e: vec){
-        if(target-e >= 0)
-            m_c = min(m_c, 1+min_changes(vec, target-e));
+    vector<int> min_coins(target+1, target+1);
+
+    min_coins[0] = 0;
+
+    for(int value = 0; value <= target; ++value)
+    {
+        for(auto coin: coins)
+        {
+            if(coin <= value)
+            {
+                min_coins[value] = min(min_coins[value], 1+min_coins[value-coin]);
+            }
+        }
     }
-    return m_c;
+
+    return min_coins[target];
+}
+
+int main() {
+    vector<int> coins {9, 6, 5, 1};
+    cout << minimum_coin_change(coins, 11) << '\n';
+    return 0;
 }
