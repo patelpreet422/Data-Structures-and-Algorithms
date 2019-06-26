aa95eba04c4a89e8ff777b3f10c3baf368aad052
Preet Patel <patelpreet422@gmail.com>
2019-06-26 18:43:27 +0530

    Update coin_change_matrix.cpp

--- coin_change_matrix.cpp
+++ coin_change_matrix.cpp
@@ -1,55 +1,37 @@
-long getWays(long target, vector<long> coins)
+#include <iostream>
+#include <vector>
+using namespace std;
+
+long getWays(vector<long> coins, int target)
 {
-    long ways[coins.size()][target+1];
-    ways[0][0] = 1;
+    vector<vector<long>> ways(coins.size()+1, vector<long>(target+1, 0));
+
     // fill first column with 1
-    for(int i = 0; i < coins.size(); ++i)
+    for(int i = 0; i <= coins.size(); ++i)
     {
         ways[i][0] = 1;
     }
-    
-    // fill first row
-    for(int j = 1; j <= target; ++j)
-    {
-        if(j < coins[0])
-        {
-            ways[0][j] = 0;
-        } else {
-            if(j % coins[0] == 0)
-                ways[0][j] = 1;
-            else
-                ways[0][j] = 0;
-        }
-    }
-    for(int i = 1; i < coins.size(); ++i)
+
+    for(int i = 1; i <= coins.size(); ++i)
     {
         for(int j = 1; j <= target; ++j)
         {
-            if(j < coins[i])
+            if(j-coins[i-1] < 0)
             {
                 ways[i][j] = ways[i-1][j];
             } else {
-                ways[i][j] = ways[i-1][j] + ways[i][j - coins[i]];
+                ways[i][j] = ways[i-1][j] + ways[i][j - coins[i-1]];
             }
         }
     }
-    /*
-    // print
-    cout << "    ";
-    for(int j = 0; j <= target; ++j)
-    {
-        cout << j << " ";
-    }
-    cout << "\n----------------------\n";
-    for(int i = 0; i < coins.size(); ++i)
-    {
-        cout << coins[i] << " | ";
-        for(int j = 0; j <= target; ++j)
-        {
-            cout << ways[i][j] << " ";
-        }
-        cout << '\n';
-    }
-    */
-    return ways[coins.size()-1][target];
+    return ways[coins.size()][target];
+}
+
+int main() {
+    vector<long> coins{1, 2, 5};
+    string chosen = "";
+    int ans = getWays(coins, 5);
+
+    cout << ans << '\n';
+    return 0;
 }
