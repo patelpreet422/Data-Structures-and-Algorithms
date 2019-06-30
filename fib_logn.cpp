eb1e680f97781697c291a110874d3e280494f355
Preet Patel <patelpreet422@gmail.com>
2019-07-01 00:33:35 +0530

    Create fib_logn.cpp

---
+++ fib_logn.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <initializer_list>
+#include <unordered_map>
+#include <exception>
+#include <stdexcept>
+#include <math.h>
+#include <algorithm>
+
+using namespace std;
+template <typename T>
+using matrix = vector<vector<T>>;
+const int64_t MOD = 1'000'000'007;
+
+void multiply(matrix<int64_t>& m, matrix<int64_t>& b)
+{
+    matrix<int64_t> t{{0, 0}, {0, 0}};
+    for(int i = 0; i < 2; ++i)
+    {
+        for(int j = 0; j < 2; ++j)
+        {
+            for(int k = 0; k < 2; ++k)
+                t[i][j] = (t[i][j] + m[i][k]*b[k][j])%MOD;
+        }
+    }
+    m = t;
+}
+
+matrix<int64_t> matrix_pow(matrix<int64_t> m, int64_t k)
+{
+    matrix<int64_t> r = {{1, 0}, {0, 1}};
+    for(; k; k >>= 1)
+    {
+        if(k&1) multiply(r, m);
+        multiply(m, m);
+    }
+    return r;
+}
+
+int64_t fib(int64_t k)
+{
+    if(k <= 1) return k;
+    matrix<int64_t> m {
+        {1, 1},
+        {1, 0}
+    };
+    m = matrix_pow(m, k);
+    return m[0][1];
+}
+
+int64_t sum_fib_n(int64_t k)
+{
+    return fib(k+2) - 1;
+}
+
+int main()
+{
+    cout << fib(10) << '\n';
+    return 0;
+}
