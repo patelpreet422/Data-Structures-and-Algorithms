ceb14e8212b9635f6d65925926e9d9a305cd6738
Preet Patel <patelpreet422@gmail.com>
2019-08-22 23:50:01 +0530

    Update fenwick.cpp

--- fenwick.cpp
+++ fenwick.cpp
@@ -2,49 +2,49 @@
 #include <vector>
 using namespace std;
 
-//0-based indexing
+template <typename T>
 struct Fenwick
 {
-	vector<int> bit;
-	Fenwick() = delete;
-	Fenwick(size_t s)
-	{
-		bit.resize(s+1, 0);
-	}
+    vector<T> bit;
+    Fenwick() = delete;
+    explicit Fenwick(size_t s)
+    {
+        bit.resize(s+1, 0);
+    }
 
-	//O(n) construction
-	Fenwick(const vector<int>& vec)
-	{
-		bit = {0};
-		bit.insert(bit.begin()+1, vec.begin(), vec.end());
-		for(size_t i = 1; i <= vec.size(); ++i)
-		{
-			int parent = i + (i&-i);
-			if(parent <= vec.size())
-				bit[parent] += bit[i];
-		}
-	}
+    //O(n) construction
+    Fenwick(const vector<int>& vec)
+    {
+        bit = {0};
+        bit.insert(bit.begin()+1, vec.begin(), vec.end());
+        for(size_t i = 1; i <= vec.size(); ++i)
+        {
+            int64_t parent = i + (i&-i);
+            if(parent <= vec.size())
+                bit[parent] += bit[i];
+        }
+    }
 
-	int sum(int i)
-	{
-		int res = 0;
-		++i;
-		for(; i > 0; i -= (i&-i))
-			res += bit[i];
-		return res;
-	}
+    T sum(int i)
+    {
+        T res = 0;
+        ++i;
+        for(; i > 0; i -= (i&-i))
+            res += bit[i];
+        return res;
+    }
 
-	int sum(int i, int j)
-	{
-		return sum(j) - sum(i-1);
-	}
+    T sum(int i, int j)
+    {
+        return sum(j) - sum(i-1);
+    }
 
-	void add(int i, int delta)
-	{
-		++i;
-		for(; i <= bit.size(); i += (i&-i))
-			bit[i] += delta;
-	}
+    void add(int i, int64_t delta)
+    {
+        ++i;
+        for(; i <= bit.size(); i += (i&-i))
+            bit[i] += delta;
+    }
 };
 
 int main()
