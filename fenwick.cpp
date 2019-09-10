e2aa4d5209066412a6e71c8e612b8a01f83caf41
Preet Patel <patelpreet422@gmail.com>
2019-09-11 02:21:03 +0530

    Add files via upload

--- fenwick.cpp
+++ fenwick.cpp
@@ -1,93 +1,118 @@
 #include <iostream>
+#include <cmath>
 #include <vector>
 using namespace std;
 
 template <typename T>
 struct Fenwick
 {
-    vector<T> bit;
-    Fenwick() = delete;
-    explicit Fenwick(size_t s)
-    {
-        bit.resize(s+1, 0);
-    }
+  vector<T> bit;
+  Fenwick() = delete;
+  explicit Fenwick(size_t s)
+  {
+    bit.resize(s+1, 0);
+  }
 
-    //O(n) construction
-    explicit Fenwick(const vector<T>& vec)
+  //O(n) construction
+  explicit Fenwick(const vector<T>& vec)
+  {
+    bit = {0};
+    bit.insert(bit.begin()+1, vec.begin(), vec.end());
+    for(size_t i = 1; i <= vec.size(); ++i)
     {
-        bit = {0};
-        bit.insert(bit.begin()+1, vec.begin(), vec.end());
-        for(size_t i = 1; i <= vec.size(); ++i)
-        {
-            int64_t parent = i + (i&-i);
-            if(parent <= vec.size())
-                bit[parent] += bit[i];
-        }
+      int64_t parent = i + (i&-i);
+      if(parent <= vec.size())
+        bit[parent] += bit[i];
     }
+  }
 
-    //initilizer_list ctor
-    Fenwick(initializer_list<T> it)
+  //initilizer_list ctor
+  Fenwick(initializer_list<T> it)
+  {
+    bit = {0};
+    bit.insert(bit.begin()+1, it.begin(), it.end());
+    for(size_t i = 1; i <= it.size(); ++i)
     {
-        bit = {0};
-        bit.insert(bit.begin()+1, it.begin(), it.end());
-        for(size_t i = 1; i <= it.size(); ++i)
-        {
-            int64_t parent = i + (i&-i);
-            if(parent <= it.size())
-                bit[parent] += bit[i];
-        }
+      int64_t parent = i + (i&-i);
+      if(parent <= it.size())
+        bit[parent] += bit[i];
     }
+  }
 
-    T sum(T i)
+  T sum(T i)
+  {
+    T res = 0;
+    ++i;
+    for(; i > 0; i -= (i&-i))
+      res += bit[i];
+    return res;
+  }
+
+  T sum(size_t i, size_t j)
+  {
+    return sum(j) - sum(i-1);
+  }
+
+  void add(size_t i, T delta)
+  {
+    ++i;
+    for(; i <= bit.size(); i += (i&-i))
+      bit[i] += delta;
+  }
+
+  // O(log(n)*log(n)) lower bound 
+  // finds lower_bound of v prefix_sum of vector 
+  int lower_bound_(int v)
+  {
+    int l = 0, h = bit.size()-1;
+    while(l<h)
     {
-        T res = 0;
-        ++i;
-        for(; i > 0; i -= (i&-i))
-            res += bit[i];
-        return res;
+      int m =(l+h)/2;
+      if(v<=sum(m)) h = m;
+      else l = m+1;
     }
+    return l;
+  }
 
-    T sum(size_t i, size_t j)
+  // O(log(n)) 
+  // this is similar to meta_binary_search algorithm
+  int lower_bound(int v)
+  {
+    int sum = 0;
+    int p = 0;
+    int msb = ceil(log2(bit.size()))-1;
+    while(msb>=0)
     {
-        return sum(j) - sum(i-1);
-    }
-
-    void add(size_t i, T delta)
-    {
-        ++i;
-        for(; i <= bit.size(); i += (i&-i))
-            bit[i] += delta;
-    }
-
-    // O(log(n)*log(n)) lower bound 
-    // finds lower_bound of v prefix_sum of vector 
-    int lower_bound(int v)
-    {
-      int l = 0, h = bit.size()-1;
-      while(l<h)
+      int np = p | (1 << msb);
+      if(np < bit.size() && sum+bit[np]<v)
       {
-        int m =(l+h)/2;
-        if(v<=sum(m)) h = m;
-        else l = m+1;
+        sum += bit[np];
+        p = np;
       }
-      return l;
+      --msb;
     }
+    return p;
+  }
 
-    friend ostream& operator<<(ostream& os, const Fenwick<T>& bit)
-    {
-        for(auto e: bit) os << e << " ";
-        return os;
-    }
+  friend ostream& operator<<(ostream& os, const Fenwick<T>& bit)
+  {
+    for(auto e: bit) os << e << " ";
+    return os;
+  }
 
-    typename vector<T>::iterator begin() {  return bit.begin(); }
-    typename vector<T>::iterator end() { return bit.end(); }
+  typename vector<T>::iterator begin() {  return bit.begin(); }
+  typename vector<T>::iterator end() { return bit.end(); }
 
 };
 
 int main()
 {
-	vector<int> vec {8, 2, 10, 100, 1, 2};
-	Fenwick<int> t(vec);
-  cout << t.lower_bound(8) << '\n';
-	return 0;
+  vector<int> vec {8, 2, 10, 100, 1, 2};
+  Fenwick<int> t(vec);
+  while(true)
+  {
+    int n; cin >> n;
+    cout << t.lower_bound(n) << '\n';
+  }
+  return 0;
 }
