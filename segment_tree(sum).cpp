e8249da56f8fd8878a694d4bd4ef546c404da841
Preet Patel <patelpreet422@gmail.com>
2019-09-15 20:46:13 +0530

    Update segment_tree(sum).cpp

--- segment_tree(sum).cpp
+++ segment_tree(sum).cpp
@@ -1,133 +1,103 @@
-#include <cstdio>
 #include <iostream>
 #include <vector>
 #include <limits>
 
 using namespace std;
 
-struct segment_tree
-{
-    vector<int64_t> lo, hi, delta, data, raw_data;
-    explicit segment_tree(vector<int64_t> vec)
-    {
-        raw_data = std::move(vec);
-        auto n = raw_data.size();
-        lo = vector<int64_t>(4*n+1, -1);
-        hi = vector<int64_t>(4*n+1, -1);
-        delta = vector<int64_t>(4*n+1, 0);
-        data = vector<int64_t>(4*n+1, 0);
-        init(1, 0, n-1);
-        build(1, 0, n-1);
-    }
+vector<int64_t> tree;
+vector<int64_t> delta;
 
-    void init(int64_t n, int64_t l, int64_t r)
-    {
-        if(l == r) {
-            lo[n] = hi[n] = l;
-        } else {
-            int64_t mid = (l+r)/2;
-            lo[n] = l;
-            hi[n] = r;
-            init(2*n, l, mid);
-            init(2*n+1, mid+1, r);
-        }
+void make(const vector<int64_t>& vec, int64_t lo_n, int64_t hi_n, int64_t n = 0) {
+    if(lo_n == hi_n) {
+        tree[n] = vec[lo_n];
+    } else {
+        auto m = (lo_n + hi_n)/2;
+        make(vec, lo_n, m, 2*n+1);
+        make(vec, m+1, hi_n, 2*n+2);
+        tree[n] = tree[2*n+1] + tree[2*n+2];
     }
+}
 
-    void update(int64_t n)
-    {
-        // operation to be performed min, max, plus, multiplies, xor, ...
-        data[n] = data[2*n]+delta[2*n] + data[2*n+1]+delta[2*n+1];
-    }
-
-    void prop(int64_t n)
-    {
-        // how to aggregate value?
-        delta[2*n] += delta[n];
+int64_t query(int64_t lo_n, int64_t hi_n, int64_t l, int64_t r, int64_t n = 0) {
+    tree[n] += (hi_n - lo_n + 1)*delta[n];
+    if(lo_n != hi_n) {
         delta[2*n+1] += delta[n];
+        delta[2*n+2] += delta[n];
+    }
+    delta[n] = 0;
+
+    if(r < lo_n || l > hi_n) return 0;
+    else if(l <= lo_n && hi_n <= r) return tree[n];
+    auto m = (lo_n + hi_n)/2;
+
+    auto ls = query(lo_n, m, l, r, 2*n+1);
+    auto rs = query(m+1, hi_n, l, r, 2*n+2);
+
+    return ls+rs;
+}
+
+int64_t query(int64_t l, int64_t r, int64_t size) {
+    return query(0, size-1, l, r);
+}
+
+void increment(int64_t del, int64_t lo_n, int64_t hi_n, int64_t l, int64_t r, int64_t n = 0) {
+    tree[n] += (hi_n - lo_n + 1) * delta[n];
+    if (lo_n != hi_n) {
+        delta[2 * n + 1] += delta[n];
+        delta[2 * n + 2] += delta[n];
+    }
+    delta[n] = 0;
+
+    if(r < lo_n || l > hi_n) return;
+    else if(l <= lo_n && hi_n <= r) {
+        tree[n] += (hi_n - lo_n + 1) * del;
+        if (lo_n != hi_n) {
+            delta[2 * n + 1] += del;
+            delta[2 * n + 2] += del;
+        }
         delta[n] = 0;
     }
+    else {
+        auto m = (lo_n + hi_n)/2;
 
-    void build(int64_t n, int64_t l, int64_t r)
-    {
-        if(l == r) {
-            data[n] = raw_data[l];
-        } else {
-            int64_t mid = (l+r)/2;
-            build(2*n, l, mid);
-            build(2*n+1, mid+1, r);
-            //data[n] = max(data[2*n], data[2*n+1]);
-            update(n);
-        }
+        increment(del, lo_n, m, l, r, 2*n+1);
+        increment(del, m+1, hi_n, l, r, 2*n+2);
+
+        tree[n] = tree[2*n+1] + tree[2*n+2];
     }
+}
 
-    int64_t query(int64_t n, int64_t l, int64_t r)
-    {
-        // partial overlap
-        if(r < lo[n] || hi[n] < l)
-        {
-            // identity of operation man, min, plus, multiples, xor, ...
-            return 0;
-        }
-        // total overlap
-        if(l <= lo[n] && hi[n] <= r)
-        {
-            // how to aggregate values?
-            return data[n]+delta[n];
-        }
-        prop(n);
-        int64_t lr = query(2*n, l, r);
-        int64_t rr = query(2*n+1, l, r);
-        update(n);
-        return lr + rr;
-    }
+void increment(int64_t del, int64_t l, int64_t r, int64_t size) {
+    increment(del, 0, size-1, l, r);
+}
 
-    int64_t query(int64_t l, int64_t r)
-    {
-        return query(1, l, r);
-    }
+void init(const vector<int64_t>& vec) {
+    tree = vector<int64_t>(4*vec.size()+1, 0);
+    delta = vector<int64_t>(4*vec.size()+1, 0);
+    make(vec, 0, vec.size()-1);
+}
 
-    void increment(int64_t n, int64_t l, int64_t r, int64_t del)
-    {
-        // partial overlap
-        if(r < lo[n] || hi[n] < l) return;
-        // total overlap
-        if(l <= lo[n] && hi[n] <= r) {
-            // how to aggregate values?
-            delta[n] += del;
-            return;
-        }
-
-        prop(n);
-        increment(2*n, l, r, del);
-        increment(2*n+1, l, r, del);
-        update(n);
-    }
-
-    void increment(int64_t l, int64_t r, int64_t del)
-    {
-        increment(1, l, r, del);
-    }
-};
-
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
     cout.tie(nullptr);
-    int64_t n, q; cin >> n >> q;
-    vector<int64_t> vec(n);
-    for(auto& e: vec) cin >> e;
-    segment_tree st(vec);
-
-    while(q--) {
-        int64_t l, r; cin >> l >> r;
-        st.increment(l, l, -vec[l]+r);
-        vec[l] = r;
-    }
-
-    for(int i = 0; i < vec.size(); ++i) {
-        for(int j = i; j < vec.size(); ++j) {
-            cout << "sum[" << i << ", " << j << "]: " << st.query(i, j) << '\n';
+    int64_t t; cin >> t;
+    while(t--) {
+        size_t n, c; cin >> n >> c;
+        tree = vector<int64_t>(4*n+1, 0);
+        delta = vector<int64_t>(4*n+1, 0);
+        while(c--) {
+            int64_t o;
+            cin >> o;
+            if (o == 0) {
+                int64_t p, q, v;
+                cin >> p >> q >> v;
+                increment(v, p-1, q-1, n);
+            } else {
+                int64_t p, q;
+                cin >> p >> q;
+                cout << query(p-1, q-1, n) << '\n';
+            }
         }
     }
     return 0;
