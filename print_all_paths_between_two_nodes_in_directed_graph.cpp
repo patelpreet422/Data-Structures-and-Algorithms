3ca42d6a8588bfdbe967c5c39806bd502b37fe78
Preet Patel <patelpreet422@gmail.com>
2019-10-25 01:43:02 +0530

    Create print_all_paths_between_two_nodes_in_directed_graph.cpp

---
+++ print_all_paths_between_two_nodes_in_directed_graph.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <deque>
+using namespace std;
+
+vector<vector<int>> g;
+vector<bool> being_visited;
+vector<vector<int>> paths;
+
+deque<int> path;
+
+void dfs(int v, int d) {
+  being_visited[v] = true;
+  path.push_back(v);
+
+  if(v == d) {
+    paths.push_back(vector<int>(begin(path), end(path)));
+  } else {
+    for(auto c: g[v]) {
+      if(!being_visited[c]) {
+        dfs(c, d);
+      }
+    }
+  }
+
+  being_visited[v] = false;
+  path.pop_back();
+}
+
+int main() {
+  int n, m; cin >> n >> m;
+  g.assign(n, vector<int>());
+  being_visited.assign(n, false);
+  for(int i = 1; i <= m; ++i) {
+    int u, v; cin >> u >> v; --u; --v;
+    g[u].push_back(v);
+  }
+
+  int v = 0, d = g.size()-1;
+  dfs(v, d);
+
+  for(auto& r: paths) {
+    for(auto e: r) cout << e << ' ';
+    cout << '\n';
+  }
+
+  return 0;
+}
