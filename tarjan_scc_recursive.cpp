e44db3bdb30c75f7ad7c1fa72d62627015c23634
Preet Patel <patelpreet422@gmail.com>
2019-11-02 14:16:43 +0530

    Update tarjan_scc_recursive.cpp

--- tarjan_scc_recursive.cpp
+++ tarjan_scc_recursive.cpp
@@ -1,64 +1,55 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 #include <stack>
-#include <climits>
 using namespace std;
-using graph_t = vector<vector<int>>;
 
-void dfs(graph_t& g, int v, stack<int>& s, vector<bool>& on_stack, vector<int>& id, vector<int>& low_link, int& id_num)
-{
-  s.push(v);
-  on_stack[v] = true;
-  id[v] = low_link[v] = id_num++;
-  for(auto c: g[v]) {
-    if(id[c] == -1) {
-      dfs(g, c, s, on_stack, id, low_link, id_num);
-    }
-    if(on_stack[c]) {
-      low_link[v] = min(low_link[v], low_link[c]);
-    }
+vector<vector<int>> g;
+vector<bool> visited, on_stack;
+vector<int> id, low;
+stack<int> s;
+int c = 0;
+
+void dfs(int n) {
+  s.push(n); visited[n] = on_stack[n] = true;
+  low[n] = id[n] = c++;
+  for(auto c: g[n]) {
+    if(!visited[c]) dfs(c);
+    if(on_stack[c]) low[n] = min(low[n], low[c]);
   }
 
-  if(id[v] == low_link[v]) {
+  if(id[n] == low[n]) {
     while(true) {
-      int curr = s.top();
-      s.pop();
-      on_stack[curr] = false;
-      low_link[curr] = low_link[v];
-      if(curr == v) break;
+      int t = s.top(); s.pop(); on_stack[t] = false;
+      low[t] = low[n];
+      if(t == n) break;
     }
   }
 }
 
-void scc(graph_t& g)
-{
-  int n = g.size();
-  int id_num = 0;
-  stack<int> s;
-  vector<bool> on_stack(n, false);
-  vector<int> id(n, -1), low_link(n, numeric_limits<int>::max());
-  for(int i=0; i<n; ++i)
-  {
-    if(id[i]==-1)
-      dfs(g, i, s, on_stack, id, low_link, id_num);
-  }
-  for(int i=0; i<n; ++i)
-  {
-    cout << "ll[" << i << "]: " << low_link[i] << '\n';
+void scc() {
+  for(int i = 0; i < g.size(); ++i) {
+    if(!visited[i]) dfs(i);
   }
 }
 
-int main()
-{
+int main() {
   int n, m; cin >> n >> m;
-  graph_t g(n);
-  for(int i=1; i<=m; ++i)
-  {
+  g.assign(n, vector<int>());
+  visited.assign(n, false);
+  on_stack.assign(n, false);
+  id.assign(n, -1);
+  low.assign(n ,-1);
+
+  for(int i = 0; i < m; ++i) {
     int v, u; cin >> v >> u;
     g[v].push_back(u);
   }
 
-  scc(g);
+  scc();
+
+  for(int i = 0; i < n; ++i) {
+    cout << "low[" << i << "]: " << low[i] << '\n';
+  }
+
   return 0;
 }
-  
