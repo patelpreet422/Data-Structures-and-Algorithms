8d1499cf14f721b4351b7ec69c87c0635b1b71fa
Preet Patel <patelpreet422@gmail.com>
2019-11-28 00:55:56 +0530

    Update efficient_segment_tree.cpp

--- efficient_segment_tree.cpp
+++ efficient_segment_tree.cpp
@@ -45,7 +45,7 @@ int query(int l) {
 }
 
 void modify(int l, int del) { // modify[l, l]
-    l = n;
+    l += n;
     tree[l] += del;
     while(l > 1) {
         tree[l>>1] = tree[l] + tree[l ^ 1];
