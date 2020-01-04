f12261c86ceca0f816ee61c795c8808b7262f91b
Preet Patel <patelpreet422@gmail.com>
2020-01-04 23:49:32 +0530

    Update inversion_fenwick.cpp

--- inversion_fenwick.cpp
+++ inversion_fenwick.cpp
@@ -1,91 +1,51 @@
-#include <iostream>
-#include <vector>
-#include <unordered_map>
-#include <iterator>
 #include <algorithm>
-#include <numeric>
-using namespace std;
-
 #include <iostream>
+#include <iterator>
 #include <vector>
 using namespace std;
 
-//0-based indexing
-template <typename T>
-struct Fenwick
-{
-    vector<T> bit;
-    Fenwick() = delete;
-    explicit Fenwick(size_t s)
-    {
-        bit.resize(s+1, 0);
-    }
+vector<int64_t> tree;
 
-    //O(n) construction
-    Fenwick(const vector<int>& vec)
-    {
-        bit = {0};
-        bit.insert(bit.begin()+1, vec.begin(), vec.end());
-        for(size_t i = 1; i <= vec.size(); ++i)
-        {
-            int64_t parent = i + (i&-i);
-            if(parent <= vec.size())
-                bit[parent] += bit[i];
-        }
-    }
-
-    T sum(int i)
-    {
-        T res = 0;
-        ++i;
-        for(; i > 0; i -= (i&-i))
-            res += bit[i];
-        return res;
-    }
-
-    T sum(int i, int j)
-    {
-        return sum(j) - sum(i-1);
-    }
-
-    void add(int i, int64_t delta)
-    {
-        ++i;
-        for(; i <= bit.size(); i += (i&-i))
-            bit[i] += delta;
-    }
-};
-
-int64_t inversion(vector<int64_t> arr)
-{
-    int64_t s = 0;
-    auto vec = arr;
-    sort(begin(vec), end(vec));
-    unordered_map<int64_t, int> index;
-    Fenwick<int64_t> bit(vec.size());
-
-    for(int i = 0; i < vec.size(); ++i)
-        index[vec[i]] = i;
-
-    for(auto e: arr)
-    {
-        auto idx = index[e];
-        s += bit.sum(idx, vec.size()-1);
-        bit.add(idx, 1);
-    }
-
-    return s;
+void add(int64_t i) {
+  for (++i; i < tree.size(); i += (i & -i)) {
+    tree[i] += 1;
+  }
 }
 
-int main()
-{
-    int64_t t; cin >> t;
-    while(t--)
-    {
-        int64_t n; cin >> n;
-        vector<int64_t> vec(n);
-        for(auto& e: vec) cin >> e;
-        cout << inversion(vec) << '\n';
-    }
-    return 0;
+int64_t sum(int64_t i) {
+  int64_t r = 0;
+  for (++i; i; i -= (i & -i)) {
+    r += tree[i];
+  }
+  return r;
+}
+
+int64_t inversion(vector<int64_t> &a) {
+  int64_t n = a.size();
+
+  vector<int64_t> b = a;
+  sort(begin(b), end(b));
+    
+  // convert a to compressed version maint64_taining the relative order of
+  // elements in the original array
+  // use coordinate compression to reduce the size of the fenwick tree
+  for (int64_t i = 0; i < n; ++i) {
+    a[i] = lower_bound(begin(b), end(b), a[i]) - begin(b);
+  }
+
+  int64_t r = 0;
+  tree.assign(n + 1, 0);
+  for (int64_t i = n - 1; i >= 0; --i) {
+    r += sum(a[i]);
+    add(a[i]);
+  }
+
+  return r;
+}
+
+int main() {
+  vector<int64_t> a;
+  copy(istream_iterator<int64_t>(cin), istream_iterator<int64_t>(),
+  back_inserter(a)); cout << inversion(a) << '\n';
+  return 0;
 }
