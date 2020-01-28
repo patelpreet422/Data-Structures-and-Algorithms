d1c64a939138f5a40a582c53058df42c4b65b173
Preet Patel <patelpreet422@gmail.com>
2020-01-28 23:35:19 +0530

    Update kadane.cpp

--- kadane.cpp
+++ kadane.cpp
@@ -1,33 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int max_subarray_sum(vector<int> vec)
+int max_subarray_sum(vector<int>& A)
 {
-    int s_idx = 0, e_idx = 0;
-    int max_s = 0;
-    int s = 0;
-    for(int i = 0; i < vec.size(); ++i)
-    {
-        s += vec[i];
-        if(s < 0)
-        {
-            s = 0;
-            s_idx = i+1;
-        }
-        if(max_s < s)
-        {
-            max_s = s;
-            e_idx = i;
-        }
+    bool all_negative = true;
+    
+    for(auto e: A) {
+        all_negative &= (e < 0);
     }
-
-    // print range
-    for(int i = s_idx; i <= e_idx; ++i) cout << vec[i] << " ";
-    cout << '\n';
-
-    return max_s;
+    
+    if(all_negative) {
+        return *max_element(begin(A), end(A));
+    }
+    
+    int s = 0;
+    int rs = 0;
+    int n = A.size();
+    for(int i = 0; i < n; ++i) {
+        rs += A[i];
+        if(rs < 0) {
+            rs = 0;
+        }
+        s = max(s, rs);
+    }
+    return s;
 }
 
 int main() {
