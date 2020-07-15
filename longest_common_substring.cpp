e54b99e6ad4206e22d82b9a407c48c7ad2d16bf1
Preet Patel <patelpreet422@gmail.com>
2020-07-16 02:26:47 +0530

    Store starting index instead of entire string

--- longest_common_substring.cpp
+++ longest_common_substring.cpp
@@ -6,28 +6,28 @@ class Solution {
 public:
   string a; 
   string b;
-  int lcs(int i, int j, int l, string &chosen) {
+  int lcs(int i, int j, int l, int &s) {
     if (i == -1 or j == -1) {
       return l;
     }
 
     int ans = 0;
     if (a[i] == b[j]) {
-      chosen.insert(chosen.begin(), a[i]);
-      ans = lcs(i - 1, j - 1, l + 1, chosen);
+      s = i;
+      ans = lcs(i - 1, j - 1, l + 1, s);
     }
-    string ls = "";
-    string rs = "";
-    int lx = lcs(i - 1, j, 0, ls);
-    int ly = lcs(i, j - 1, 0, rs);
+
+    int xi = -1, yi = 1;
+    int lx = lcs(i - 1, j, 0, xi);
+    int ly = lcs(i, j - 1, 0, yi);
     if (ans < lx) {
       ans = lx;
-      chosen = ls;
+      s = xi;
     }
 
     if (ans < ly) {
       ans = ly;
-      chosen = rs;
+      s = yi;
     }
     return ans;
   }
@@ -36,17 +36,17 @@ public:
     this->a = a;
     this->b = b;
 
-    string str;
-    int lc = lcs(a.size() - 1, b.size() - 1, 0, str);
-    cout << str << '\n';
+    int s = -1;
+    int lc = lcs(a.size() - 1, b.size() - 1, 0, s);
+    cout << a.substr(s, lc) << '\n';
     return lc;
   }
 };
 
 int main() {
   Solution solution;
-  string a = "gee";
-  string b = "xgee";
+  string a = "xgeeksforgeeks";
+  string b = "geeksforgeeks";
   cout << solution.lcs(a, b) << '\n';
   return 0;
 }
