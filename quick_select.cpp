f65f4609168ebca4c4c6fa601ad4db8188f3247c
Preet Patel <patelpreet422@gmail.com>
2020-07-25 03:10:34 +0530

    Update quick_select.cpp

--- quick_select.cpp
+++ quick_select.cpp
@@ -21,25 +21,28 @@ int quick_select(vector<int> &v, int l, int h, int k) {
   if (l > h)
     return -1;
   int p = partition(v, l, h);
-  if (k == p - l)
+  if (k == p)
     return p;
-  if (k < p - l)
+  if (k < p)
     return quick_select(v, l, p - 1, k);
 
-  return quick_select(v, p + 1, h, k - p + l - 1);
+  return quick_select(v, p + 1, h, k);
 }
 
 int main() {
-  vector<int> v{1, 2};
+  vector<int> v{1, 2, 3, 4, 5};
   do {
     auto t = v;
-    cout << "vec: ";
-    for (auto e : t) {
-      cout << e << ' ';
+    /* cout << "vec: "; */
+    /* for (auto e : t) { */
+    /*   cout << e << ' '; */
+    /* } */
+    /* cout << '\n'; */
+    cout << "quick_select: ";
+    for (int i = 0; i < t.size(); ++i) {
+      cout << t[quick_select(t, 0, t.size() - 1, i)] << ' ';
     }
     cout << '\n';
-    cout << t[quick_select(t, 0, t.size() - 1, t.size() / 2 - 1)] << ' ';
-    cout << t[quick_select(t, 0, t.size() - 1, t.size() / 2)] << '\n';
   } while (next_permutation(begin(v), end(v)));
   return 0;
 }
