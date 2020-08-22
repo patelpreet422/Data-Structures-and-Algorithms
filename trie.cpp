f6dca576984bcf72811d468e24339c7441bf8f85
Preet Patel <patelpreet422@gmail.com>
2020-08-23 01:49:19 +0530

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -1,83 +1,108 @@
-#include <iostream>
-#include <map>
+#include <bits/stdc++.h>
 using namespace std;
+using ll = long long;
 
 struct Trie {
-  map<char, Trie> children;
+  vector<Trie *> children;
   bool is_word = false;
 
-  bool empty() { return children.empty(); }
+  Trie() { this->children.assign(256, nullptr); }
 
-  void insert(const string &s) { insert(s, 0); }
+  ~Trie() {
+    for(auto& c: children) {
+      delete c;
+      c = nullptr;
+    }
+  }
 
-  void insert(const string &s, int i) {
+  bool empty() {
+    for (char i = 0; i < this->children.size(); ++i) {
+      if (this->children[i] != nullptr) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  void insert(const string& s) {
+    Trie* curr = this;
+    for(auto c: s) {
+      if (curr->children[c] == nullptr) {
+        curr->children[c] = new Trie();
+      }
+      curr = curr->children[c];
+    }
+    curr->is_word = true;
+  }
+
+  void print(string &s) {
+    if (this->is_word) {
+      cout << s << '\n';
+    }
+    for (char c = 0; c < this->children.size(); ++c) {
+      if (this->children[c] != nullptr) {
+        s.push_back(char(c));
+        this->children[c]->print(s);
+        s.pop_back();
+      }
+    }
+  }
+
+  bool contains(const string &s) {
+    Trie *curr = this;
+    for (auto c : s) {
+      if (curr->children[c] == nullptr) {
+        return false;
+      }
+      curr = curr->children[c];
+    }
+    return curr->is_word;
+  }
+
+  void remove(const string &s, int i) {
     if (i == s.size()) {
-      is_word = true;
+      this->is_word = false;
       return;
     }
-    children[s[i]].insert(s, i + 1);
+    if (this->children[s[i]] == nullptr) {
+      return;
+    }
+    this->children[s[i]]->remove(s, i + 1);
+    if (this->children[s[i]]->empty() and !this->children[s[i]]->is_word) {
+      cout << "removing... " << s[i] << "\n";
+      delete this->children[s[i]];
+      this->children[s[i]] = nullptr;
+    }
+  }
+
+  Trie *auto_complete(const string &s) {
+    Trie *curr = this;
+    for (auto c : s) {
+      if (curr->children[c] == nullptr) {
+        return nullptr;
+      }
+      curr = curr->children[c];
+    }
+    return curr;
   }
 
   void remove(const string &s) { remove(s, 0); }
 
-  void remove(const string &s, int i) {
-    if (i == s.size()) {
-      is_word = false;
-      return;
-    }
-
-    if (children.find(s[i]) == children.end())
-      return;
-
-    children[s[i]].remove(s, i + 1);
-    if (children[s[i]].empty() and !children[s[i]].is_word) {
-      children.erase(children.find(s[i]));
-      cout << "removing: " << s[i] << '\n';
-    }
-  }
-
-  Trie next_suffixes(const string &s) { return next_suffixes(s, 0); }
-
-  Trie next_suffixes(const string &s, int i) {
-    if (i == s.size()) {
-      return *this;
-    }
-
-    if (children.find(s[i]) == children.end())
-      return Trie();
-
-    return children[s[i]].next_suffixes(s, i + 1);
-  }
-
   void print() {
-    string t;
-    print(t);
-  }
-
-  void print(string &s) {
-    if (is_word)
-      cout << s << '\n';
-
-    for (auto [c, n] : children) {
-      s.push_back(c);
-      n.print(s);
-      s.pop_back();
-    }
+    string s = "";
+    print(s);
   }
 };
 
 int main() {
   Trie trie;
-  //  trie.insert("abfgh");
-  //  trie.insert("abfgi");
-  //  trie.insert("abc");
-  //  trie.insert("ab");
-  //  trie.insert("abdef");
-  //  trie.insert("abdefg");
-  trie.remove("x");
-
-  trie.next_suffixes("x").print();
+  vector<string> dict{"a",           "demo",    "demon",
+                      "graph",       "grapher", "!@#$%^789876543",
+                      "!@#$11234567"};
+  for(auto& s: dict) {
+    trie.insert(s);
+  }
   trie.print();
-
   return 0;
 }
+
